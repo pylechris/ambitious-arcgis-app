601a22ecd4500e97b7e3289b9a389a3be4fe7d0e
chris-pyle <pyle.chris@gmail.com>
2018-03-05 13:37:31 -0800

    broken pager

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,12 +1,31 @@
+import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    actions: {
+  // query parameters used by components
+  queryParams: ['start', 'num'],
+  start: 1,
+  num: 10,
+  // compute current page number based on start record
+// and the number of records per page
+pageNumber: computed('num', 'model.start', function () {
+  const pageSize = this.get('num');
+  const start = this.get('model.start');
+  return ((start - 1) / pageSize) + 1;
+}),
+actions: {
+      changePage (page) {
+        // calculate next start record based on
+        // the number of records per page
+        const pageSize = this.get('num');
+        const nextStart = ((page - 1) * pageSize) + 1;
+        this.set('start', nextStart);
+      },
         doSearch (q) {
-          // NOTE: don't need to pass route name b/c same route
-          this.transitionToRoute({
-            queryParams: { q }
-          });
-        }
+        // NOTE: don't need to pass route name b/c same route
+        this.transitionToRoute({
+          queryParams: { q , start: 1 }
+        });
       }
+    }
 });
--- app/routes/items.js
+++ app/routes/items.js
@@ -9,13 +9,15 @@ export default Route.extend({
   // changes to these query parameter will cause this route to
   // update the model by calling the "model()" hook again
   queryParams: {
-    q: { refreshModel: true }
+  // paging query params
+start: { refreshModel: true },
+num: { refreshModel: true },  q: { refreshModel: true }
   },
 
   // the model hook is used to fetch any data based on route parameters
   model (params) {
     const itemsService = this.get('itemsService');
     const q = params.q || '*';
-    return itemsService.search({ q });
+    return itemsService.search({ q, num: params.num, start: params.start });
   }
 });
