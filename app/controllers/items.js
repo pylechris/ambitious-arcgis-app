316e65f8d31489bdd127d99786e12e86262776e9
chris-pyle <pyle.chris@gmail.com>
2018-03-05 11:29:19 -0800

    added item service

---
+++ app/controllers/items.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    actions: {
+        doSearch (q) {
+          // NOTE: don't need to pass route name b/c same route
+          this.transitionToRoute({
+            queryParams: { q }
+          });
+        }
+      }
+});
