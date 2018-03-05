ba63a37d56e16dc6f0a35aea35bdb1215e23fbc9
chris-pyle <pyle.chris@gmail.com>
2018-03-05 13:45:14 -0800

    removed pager due to npm install error

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -8,19 +8,19 @@ export default Controller.extend({
   num: 10,
   // compute current page number based on start record
 // and the number of records per page
-pageNumber: computed('num', 'model.start', function () {
-  const pageSize = this.get('num');
-  const start = this.get('model.start');
-  return ((start - 1) / pageSize) + 1;
-}),
+// pageNumber: computed('num', 'model.start', function () {
+//   const pageSize = this.get('num');
+//   const start = this.get('model.start');
+//   return ((start - 1) / pageSize) + 1;
+// }),
 actions: {
-      changePage (page) {
-        // calculate next start record based on
-        // the number of records per page
-        const pageSize = this.get('num');
-        const nextStart = ((page - 1) * pageSize) + 1;
-        this.set('start', nextStart);
-      },
+      // changePage (page) {
+      //   // calculate next start record based on
+      //   // the number of records per page
+      //   const pageSize = this.get('num');
+      //   const nextStart = ((page - 1) * pageSize) + 1;
+      //   this.set('start', nextStart);
+      // },
         doSearch (q) {
         // NOTE: don't need to pass route name b/c same route
         this.transitionToRoute({
